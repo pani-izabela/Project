3057fb1193dec4372a6aea9fa63371290539e840
“pani-izabela <izabela.lach86@gmail.com>
2017-10-12 12:28:13 +0200

    Zadanie 18.2-bez podpunktu 2 i 4

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -1,7 +1,9 @@
 package com.crud.tasks.trello.client;
 
 import com.crud.tasks.config.CoreConfiguration;
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,8 @@ public class TrelloClient {
     private String trelloAppKey;
     @Value("${trello.app.token}")
     private String trelloToken;
+    @Value("${trello.app.username}")
+    private String trelloUsername;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -37,5 +41,17 @@ public class TrelloClient {
             return Arrays.asList(boardsResponse);
         }
         return new ArrayList<>();
+    }
+
+    public CreatedTrelloCard createNewCard(TrelloCardDto trelloCardDto){
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/cards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("name", trelloCardDto.getName())
+                .queryParam("desc", trelloCardDto.getDescription())
+                .queryParam("pos", trelloCardDto.getPos())
+                .queryParam("idList", trelloCardDto.getListId()).build().encode().toUri();
+
+        return restTemplate.postForObject(url, null, CreatedTrelloCard.class);
     }
 }
