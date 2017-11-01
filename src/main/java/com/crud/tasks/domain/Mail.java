faa57d18844fa50fbce498baefc6b2edbf8a3a9d
“pani-izabela <izabela.lach86@gmail.com>
2017-11-01 08:52:40 +0100

    Submoduł 19.3-przerobiony

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,6 +1,10 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class Mail {
     private String mailTo;
     private String subject;
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,11 +1,15 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Mail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SimpleEmailService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class());
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
     @Autowired
     private JavaMailSender javaMailSender;
 
@@ -21,10 +25,10 @@ public class SimpleEmailService {
 
     }
 
-    private SimpleMailMesage createMailMessage(final Mail mail){
+    private SimpleMailMessage createMailMessage(final Mail mail){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
-        mailMessage.stSubject(mail.getSubject());
+        mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
 
