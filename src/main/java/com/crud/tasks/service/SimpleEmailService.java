028588bc63acdc005895d2172a92a5e012954f5d
“pani-izabela <izabela.lach86@gmail.com>
2017-11-02 21:57:26 +0100

    Zadanie 19.3-Zadanie - dodanie maila do wiadomoi (eng. CC)

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -30,9 +30,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (mail.getToCc()==null){
-            mailMessage.setCc("");
-        } else {
+        if (mail.getToCc() != null){
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
