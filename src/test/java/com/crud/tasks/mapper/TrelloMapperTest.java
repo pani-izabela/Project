dc8c90d817dce90471407c4df59a7d66c1337799
“pani-izabela <izabela.lach86@gmail.com>
2017-11-12 14:07:41 +0100

    21.1 Zadanie - Napisz test

--- src/test/java/com/crud/tasks/mapper/TrelloMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TrelloMapperTest.java
@@ -1,9 +1,6 @@
 package com.crud.tasks.mapper;
 
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloList;
-import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.domain.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -41,22 +38,86 @@ public class TrelloMapperTest {
 
     @Test
     public void testMapToBoardsDto() throws Exception {
+        //Given
+        List<TrelloBoard> trelloBoard = new ArrayList<>();
+        List<TrelloList> trelloLists = new ArrayList<>();
+        trelloLists.add(new TrelloList("11", "test list name", false));
+        trelloBoard.add(new TrelloBoard("1", "test board name", trelloLists));
+
+        TrelloMapper trelloMapper = new TrelloMapper();
+
+        //When
+        List<TrelloBoardDto> trelloBoardDtos = trelloMapper.mapToBoardsDto(trelloBoard);
+        String trelloBoardsName = trelloBoardDtos.get(0).getName();
+
+        //Then
+        assertEquals(trelloBoardsName, "test board name");
+        assertEquals(trelloBoardDtos.size(), 1);
+
     }
 
     @Test
     public void testMapToList() throws Exception {
+        //Given
+        List<TrelloListDto> trelloListDtos = new ArrayList<>();
+        trelloListDtos.add(new TrelloListDto("11", "test list name", false));
+
+        TrelloMapper trelloMapper = new TrelloMapper();
+
+        //When
+        List<TrelloList> trelloLists = trelloMapper.mapToList(trelloListDtos);
+        boolean isClosed = trelloLists.get(0).isClosed();
+
+        //Then
+        assertEquals(trelloLists.size(), 1);
+        assertFalse(isClosed);
     }
 
     @Test
     public void testMapToListDto() throws Exception {
+        //Given
+        List<TrelloList> trelloLists = new ArrayList<>();
+        trelloLists.add(new TrelloList("11", "test list name", false));
+
+        TrelloMapper trelloMapper = new TrelloMapper();
+
+        //When
+        List<TrelloListDto> trelloListDtos = trelloMapper.mapToListDto(trelloLists);
+        String listId = trelloListDtos.get(0).getId();
+
+        //Then
+        assertEquals(trelloListDtos.size(), 1);
+        assertEquals(listId, "11");
+
     }
 
     @Test
     public void testMapToCardDto() throws Exception {
+        //Given
+        TrelloCard trelloCard = new TrelloCard("test name", "test description", "test pos", "1");
+        TrelloMapper trelloMapper = new TrelloMapper();
+
+        //When
+        TrelloCardDto trelloCardDto = trelloMapper.mapToCardDto(trelloCard);
+
+        //Then
+        assertEquals(trelloCardDto.getDescription(), "test description");
+
+
     }
 
     @Test
     public void testMapToCard() throws Exception {
+        //Given
+        TrelloCardDto trelloCardDto = new TrelloCardDto("test name", "test description", "test pos", "1");
+        TrelloMapper trelloMapper = new TrelloMapper();
+
+        //When
+        TrelloCard trelloCard = trelloMapper.mapToCard(trelloCardDto);
+
+        //Then
+        assertEquals(trelloCard.getListId(), "1");
+
     }
 
 }
